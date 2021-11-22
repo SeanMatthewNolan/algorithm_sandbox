2c5304111d7fb6b2b292087ebfa685513b23e6d5
Sean Matthew Nolan <Sean.Matthew.Nolan@gmail.com>
2021-11-22 10:49:56 -0500

    Added scalar Newton's method with Armijo step

--- root_solvers/scalar/newton_raphson.py
+++ root_solvers/scalar/newton_raphson.py
@@ -6,30 +6,33 @@ from numeric_differentiation.finite_step import CentralDiff
 
 
 class NewtonRaphsonScalar(RootSolver):
-    def __init__(self, tol=1e-5, max_steps=100):
-        self.tol = tol
+    def __init__(self, abs_tol: float = 1e-5, rel_tol: float = 1e-8, max_steps: int = 100):
+        self.abs_tol = abs_tol
+        self.rel_tol = rel_tol
         self.max_steps = max_steps
 
-    def __call__(self, func: ScalarFunction, guess: float, deriv_func: Optional[ScalarFunction] = None) \
-            -> float:
+    def __call__(self, func: ScalarFunction, guess: float, deriv_func: Optional[ScalarFunction] = None) -> float:
         if deriv_func is None:
             deriv_func = CentralDiff(func)
 
         x_n = guess
-        for step_num in range(self.max_steps):
-            f_n = func(x_n)
-            if abs(f_n) < self.tol:
-                break
+        f_n = func(x_n)
+        tol = self.abs_tol + self.rel_tol * f_n
+
+        for n in range(self.max_steps + 1):
+            if abs(f_n) < tol:
+                return x_n
 
             x_n -= f_n / deriv_func(x_n)
+            f_n = func(x_n)
 
-        return x_n
+        # TODO: Add checks
+        # TODO: Consolidate code for different versions
 
 
-class DampedNewtonScalar(RootSolver):
-    def __init__(self, tol=1e-5, max_steps=100, damp_coeff=0.5):
-        self.tol = tol
-        self.max_steps = max_steps
+class DampedNewtonScalar(NewtonRaphsonScalar):
+    def __init__(self, abs_tol: float = 1e-5, rel_tol: float = 1e-8, max_steps: int = 100, damp_coeff: float = 0.5):
+        super().__init__(abs_tol=abs_tol, rel_tol=rel_tol, max_steps=max_steps)
         self.damp_coeff = damp_coeff
 
     def __call__(self, func: ScalarFunction, guess: float, deriv_func: Optional[ScalarFunction] = None) \
@@ -38,24 +41,23 @@ class DampedNewtonScalar(RootSolver):
             deriv_func = CentralDiff(func)
 
         x_n = guess
-        for step_num in range(self.max_steps):
-            f_n = func(x_n)
-            if abs(f_n) < self.tol:
-                break
+        f_n = func(x_n)
+        tol = self.abs_tol + self.rel_tol * f_n
+
+        for n in range(self.max_steps + 1):
+            if abs(f_n) < tol:
+                return x_n
 
             x_n -= self.damp_coeff * f_n / deriv_func(x_n)
+            f_n = func(x_n)
 
         return x_n
 
 
-class BoundedNewtonScalar(RootSolver):
-    def __init__(self, tol=1e-5, max_steps=100, step_bound=10.0):
-
-        self.tol = tol
-        self.max_steps = max_steps
+class BoundedNewtonScalar(NewtonRaphsonScalar):
+    def __init__(self, abs_tol: float = 1e-5, rel_tol: float = 1e-8, step_bound: float = 10.0):
+        super().__init__(abs_tol=abs_tol, rel_tol=rel_tol)
         self.step_bound = step_bound
-
-    from numpy import clip as np_clip
 
     def clip(self, raw_step):
         return min(max(raw_step, -self.step_bound), self.step_bound)
@@ -65,11 +67,55 @@ class BoundedNewtonScalar(RootSolver):
             deriv_func = CentralDiff(func)
 
         x_n = guess
-        for step_num in range(self.max_steps):
-            f_n = func(x_n)
-            if abs(f_n) < self.tol:
-                break
+        f_n = func(x_n)
+        tol = self.abs_tol + self.rel_tol * f_n
+
+        for n in range(self.max_steps + 1):
+            if abs(f_n) < tol:
+                return x_n
 
             x_n -= self.clip(f_n / deriv_func(x_n))
+            f_n = func(x_n)
+
+        return x_n
+
+
+class NewtonArmijoScalar(NewtonRaphsonScalar):
+    """
+    [1] Armijo, L. “Minimization of Functions Having Lipschitz Continuous First Partial Derivatives.”
+    Pacific Journal of Mathematics, Vol. 16, No. 1, 1966, pp. 1–3. https://doi.org/10.2140/pjm.1966.16.1.
+    [2] Kelley, C. T. Iterative Methods for Linear and Nonlinear Equations. Society for Industrial and Applied
+    Mathematics, Philadelphia, 1995.
+    """
+
+    def __init__(self, abs_tol: float = 1e-5, rel_tol: float = 1e-8, step_reduction_factor: int = 2, alpha: float = 1e-4):
+        super().__init__(abs_tol=abs_tol, rel_tol=rel_tol)
+
+        self.alpha = alpha
+        self.step_reduction_ratio = 1 / step_reduction_factor
+
+    def armijo_step(self, x_n, f_n, raw_step, func):
+        lam = 1
+        x_trial = x_n + raw_step
+        f_trial = func(x_trial)
+        abs_f_n = abs(f_n)
+
+        while abs(f_trial) > (1 - self.alpha * lam) * abs_f_n:
+            lam *= self.step_reduction_ratio
+            x_trial = x_n + lam * raw_step
+            f_trial = func(x_trial)
+
+        return x_trial, f_trial
+
+    def __call__(self, func: ScalarFunction, guess: float, deriv_func: Optional[ScalarFunction] = None) -> float:
+        if deriv_func is None:
+            deriv_func = CentralDiff(func)
+
+        x_n = guess
+        f_n = func(x_n)
+        tol = self.abs_tol + self.rel_tol * f_n
+
+        while abs(f_n) > tol:
+            x_n, f_n = self.armijo_step(x_n, f_n, -f_n / deriv_func(x_n), func)
 
         return x_n
--- root_solvers/tests/test_scalar.py
+++ root_solvers/tests/test_scalar.py
@@ -6,13 +6,16 @@ import pytest
 
 from ..root_solver_generic import RootSolver
 from .. import NewtonRaphsonScalar, DampedNewtonScalar, BoundedNewtonScalar
+from ..scalar.newton_raphson import NewtonArmijoScalar
 
-tol = 1e-5
+abs_tol = 1e-5
+rel_tol = 1e-8
 
 scalar_root_solvers = [
-    NewtonRaphsonScalar(tol=tol),
-    DampedNewtonScalar(tol=tol),
-    BoundedNewtonScalar(tol=tol)
+    NewtonRaphsonScalar(abs_tol=abs_tol, rel_tol=rel_tol),
+    DampedNewtonScalar(abs_tol=abs_tol, rel_tol=rel_tol),
+    BoundedNewtonScalar(abs_tol=abs_tol, rel_tol=rel_tol),
+    NewtonArmijoScalar(abs_tol=abs_tol, rel_tol=rel_tol)
 ]
 
 examples = [
@@ -22,9 +25,11 @@ examples = [
 ]
 
 
-@pytest.mark.parametrize('root_solver, example', product(scalar_root_solvers, examples))
+@pytest.mark.parametrize('root_solver', scalar_root_solvers)
+@pytest.mark.parametrize('example', examples)
 def test_scalar_root_solver(root_solver: RootSolver, example: Tuple[Callable, float]):
     f, x_guess = example
     x_star = root_solver(f, x_guess)
 
+    tol = abs_tol + rel_tol * f(x_guess)
     assert f(x_star) == pytest.approx(0, abs=tol)
